1f06745a782c1e35b626560f978a59c217d3932d
HenryArasaki <82346140+HenryArasaki@users.noreply.github.com>
2021-12-09 14:50:56 -0300

    Update main.js

--- main.js
+++ main.js
@@ -6,27 +6,13 @@ const loadingImg = document.getElementById("loadingImg")
 
 async function teste() {
     const bebidaAleatoria = await fetch("https:www.thecocktaildb.com/api/json/v1/1/random.php")
-    const bebidaAleatoria2 = await fetch("https:www.thecocktaildb.com/api/json/v1/1/random.php")
-    const bebidaAleatoria3 = await fetch("https:www.thecocktaildb.com/api/json/v1/1/random.php")
-    const bebidaAleatoria4 = await fetch("https:www.thecocktaildb.com/api/json/v1/1/random.php")
-    const bebidaAleatoria5 = await fetch("https:www.thecocktaildb.com/api/json/v1/1/random.php")
-    const bebidaAleatoria6 = await fetch("https:www.thecocktaildb.com/api/json/v1/1/random.php")
-    const bebidaAleatoria7 = await fetch("https:www.thecocktaildb.com/api/json/v1/1/random.php")
-    const bebidaAleatoria8 = await fetch("https:www.thecocktaildb.com/api/json/v1/1/random.php")
+
 
     const data = await bebidaAleatoria.json()
-    const data2 = await bebidaAleatoria2.json()
-    const data3 = await bebidaAleatoria3.json()
-    const data4 = await bebidaAleatoria4.json()
-    const data5 = await bebidaAleatoria5.json()
-    const data6 = await bebidaAleatoria6.json()
-    const data7 = await bebidaAleatoria7.json()
-    const data8 = await bebidaAleatoria8.json()
+
     console.log(loadingImg)
     loadingImg.remove()
-    let datas = data.drinks.concat(data2.drinks,data3.drinks,data4.drinks,data5.drinks,data6.drinks,data7.drinks,data8.drinks)
-    console.log(datas)
-    datas.forEach(element => {
+    data.forEach(element => {
         const link = document.createElement("a")
 
         link.setAttribute("href","receita.html")
